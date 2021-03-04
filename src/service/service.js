da55b3e09b5647f1757123dfe4a103b3c733e57a
Alexey Andryukevich <alexey.andryukevich@gmail.com>
2021-03-04 13:22:23 +0200

    modified file service.js

--- src/service/service.js
+++ src/service/service.js
@@ -1,8 +1,8 @@
     export const currencyService = async () => {
-        const _apiBase = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5';
-        const res = await fetch (_apiBase);
+        const _url = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5';
+        const res = await fetch (_url);
             if (!res.ok) {
-                throw new Error (`Could not featch ${_apiBase}, status: ${res.status}`);
+                throw new Error (`Could not featch ${_url}, status: ${res.status}`);
             }
             return await res.json();
     };
