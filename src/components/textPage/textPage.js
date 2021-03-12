20dd48a3c559e4c3d0a1e52fb95675fa757b5028
Aleksandr Iv <sanik.iv@gmail.com>
2021-03-12 13:07:33 +0200

    added parsing search params to textPage.js

--- src/components/textPage/textPage.js
+++ src/components/textPage/textPage.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import queryString from 'query-string';
 import './textPage.scss';
 
 export function TextPage ({location}) {
-    const searchParams = location.search;
+    const searchParams = queryString.parse(location.search);
 
     return (
       <div className='text-page'>
           <h2 className='text-title'>Текстовая страница</h2>
           <div className='text-content'>
             <p>Это простая текстовая страница для отображения параметров роута. Параметры: </p>
-            <p>- id: <span>{searchParams.slice(4, 9)}</span> </p>
-            <p>- date: <span>{searchParams.slice(15)}</span></p>
+            <p>- id: <span>{searchParams.id}</span> </p>
+            <p>- date: <span>{searchParams.date}</span></p>
             <p></p>
           </div>
       </div>
